ebf830304be4f6ace1eed6f4332a41e4b2452c26
ioanlucut <ioan.lucut88@gmail.com>
2014-11-21 20:18:44 +0100

    feat(account-form-toggle-directive): added  - Base functionality of the directive which exposes only the service and constants to the scope

---
+++ src/app/account/directives/account-form-toggle-directive.js
@@ -0,0 +1,12 @@
+/**
+ * Directive responsible for switching account forms between them.
+ */
+angular.module("account").directive("accountFormToggle", function (AccountFormToggle, ACCOUNT_STATE) {
+    return {
+        restrict: "A",
+        link:     function (scope) {
+            scope.AccountFormToggle = AccountFormToggle;
+            scope.ACCOUNT_STATE = ACCOUNT_STATE;
+        }
+    };
+});
