af218f13f40df352762f8091de8a1a0dcdfce80b
ioanlucut <ioan.lucut88@gmail.com>
2014-11-21 20:19:19 +0100

    feat(signup-controller): added  - Base functionality of the sign up action.

---
+++ src/app/account/controllers/signup-controller.js
@@ -0,0 +1,35 @@
+/**
+ * Sign up controller responsible for user sign up action.
+ */
+angular.module("account").controller("SignUpCtrl", function ($state, $scope, AuthService, AUTH_EVENTS, STATES) {
+
+    /**
+     * * Sign up user information.
+     * @type {{firstName: string, lastName: string, email: string, password: string}}
+     */
+    $scope.signUpData = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    };
+
+    /**
+     * Sign up functionality.
+     * @param signUpData
+     */
+    $scope.signUp = function (signUpData) {
+        if ($scope.signUpForm.$valid) {
+            AuthService.login(signUpData.email, signUpData.password)
+                .then(function () {
+                    setTimeout(function () {
+                        $state.go(STATES.home);
+                    }, 300);
+                });
+        }
+        else {
+            $scope.signUpForm.submitted = true;
+        }
+
+    }
+});
