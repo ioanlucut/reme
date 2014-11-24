993935890abaf99ebeafb98d97e816d847db460d
ioanlucut <ioan.lucut88@gmail.com>
2014-11-24 18:41:57 +0100

    refactor(forgot-password-controller): Added forgot action controller.

---
+++ src/app/account/controllers/forgot-password-controller.js
@@ -0,0 +1,30 @@
+/**
+ * Forgot password controller responsible for user forgot password action.
+ */
+angular
+    .module("account")
+    .controller("ForgotPasswordCtrl", function ($state, $scope, AuthService, AUTH_EVENTS, ACCOUNT_STATE, AccountFormToggle) {
+
+        /**
+         * Request password reset up user information.
+         */
+        $scope.forgotPasswordData = {
+            email: ""
+        };
+
+        /**
+         * Request password reset functionality.
+         */
+        $scope.requestPasswordReset = function () {
+            if ( $scope.forgotPasswordForm.$valid ) {
+                AuthService.requestPasswordReset($scope.forgotPasswordData.email)
+                    .then(function () {
+                        AccountFormToggle.setState(ACCOUNT_STATE.forgotPasswordEmailSent);
+                    });
+            }
+            else {
+                $scope.forgotPasswordForm.submitted = true;
+            }
+
+        }
+    });
