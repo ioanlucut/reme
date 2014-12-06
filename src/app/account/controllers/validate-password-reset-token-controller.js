280298ede4ae64b3d83b38117484b94b00b714e3
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-07 00:58:36 +0100

    feat(password reset) - refactor of the whole password reset functionality.

--- src/app/account/controllers/validate-password-reset-token-controller.js
+++ src/app/account/controllers/validate-password-reset-token-controller.js
@@ -2,16 +2,64 @@ angular
     .module("account")
     .controller("ValidatePasswordResetTokenCtrl", function ($scope, $stateParams, AuthService, StatesHandler, AccountFormToggle, ACCOUNT_FORM_STATE) {
 
-        $scope.isFeedbackToBeShown = false;
         $scope.isUserAuthenticated = AuthService.isAuthenticated();
 
-        $scope.validateTokenResult = AuthService.validatePasswordResetToken($stateParams.token)
-            .catch(function (response) {
+        $scope.isResetPasswordErrorOcurred = false;
+
+        $scope.errorMessages = "";
+
+        $scope.resetPasswordData = {
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+            token: $stateParams.token
+        };
+
+        $scope.isTokenResultValidated = AuthService.validatePasswordResetToken($scope.resetPasswordData.token)
+            .then(function (response) {
+
+                // Take the email from
+                $scope.resetPasswordData.email = response.email;
+
+                return true;
+            }).catch(function (response) {
 
                 $scope.errorMessages = response.data && response.data.errors;
-            }).finally(function () {
-                $scope.isFeedbackToBeShown = true;
+
+                return false;
             });
+
+        /**
+         * Reset password data functionality.
+         * @param resetPasswordData
+         */
+        $scope.resetPassword = function (resetPasswordData) {
+            if ( $scope.resetPasswordForm.$valid ) {
+
+                AuthService.resetPasswordWithToken(resetPasswordData.email, resetPasswordData.password, resetPasswordData.passwordConfirmation, resetPasswordData.token)
+                    .then(function () {
+                        $scope.isResetPasswordErrorOcurred = false;
+                        AccountFormToggle.setState(ACCOUNT_FORM_STATE.resetPasswordSuccessfully);
+                    })
+                    .catch(function (response) {
+                        $scope.isResetPasswordErrorOcurred = true;
+
+                        $scope.errorMessages = response.data && response.data.errors;
+
+                        if ( _.isEmpty($scope.errorMessages) ) {
+                            $scope.errorMessages = ["We encountered a small problem. Please be patient, we come back to you."]
+                        }
+
+                        // remove data from inputs
+                        $scope.resetPasswordData.newPassword = "";
+                        $scope.resetPasswordData.newPasswordConfirmation = "";
+                    });
+            }
+            else {
+                $scope.resetPasswordForm.submitted = true;
+            }
+
+        };
 
         $scope.continueToLogin = function () {
             StatesHandler.goToLogin();
