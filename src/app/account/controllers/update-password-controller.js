8a76c37ed1c96e8534b4f5c50304fb170377957c
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-06 20:47:03 +0100

    feat(profile): Add update password functionality (from profile).

---
+++ src/app/account/controllers/update-password-controller.js
@@ -0,0 +1,53 @@
+/**
+ * Update password controller.
+ */
+angular
+    .module("account")
+    .controller("UpdatePasswordCtrl", function ($scope, AuthService, ACCOUNT_FORM_STATE, AccountFormToggle) {
+
+        /**
+         * Flag which tells if the update password action went well or not.
+         * @type {boolean}
+         */
+        $scope.isUpdatePasswordErrorOcurred = false;
+
+        $scope.errorMessages = "";
+
+        /**
+         * Update password user information.
+         * @type {{oldPassword: string, newPassword: string, newPasswordConfirmation: string}}
+         */
+        $scope.updatePasswordData = {
+            oldPassword: "",
+            newPassword: "",
+            newPasswordConfirmation: ""
+        };
+
+        /**
+         * Update password data functionality.
+         * @param updatePasswordData
+         */
+        $scope.updatePassword = function (updatePasswordData) {
+            if ( $scope.updatePasswordForm.$valid ) {
+
+                AuthService.updatePassword(updatePasswordData.oldPassword, updatePasswordData.newPassword, updatePasswordData.newPasswordConfirmation)
+                    .then(function () {
+                        $scope.isUpdatePasswordErrorOcurred = false;
+                        AccountFormToggle.setState(ACCOUNT_FORM_STATE.updatePasswordSuccessfully);
+                    })
+                    .catch(function (response) {
+                        $scope.isUpdatePasswordErrorOcurred = true;
+
+                        $scope.errorMessages = response.data && response.data.errors;
+
+                        if ( _.isEmpty($scope.errorMessages) ) {
+                            $scope.errorMessages = ["We encountered a small problem. Please be patient, we come back to you."]
+                        }
+                    });
+            }
+            else {
+                $scope.updatePasswordForm.submitted = true;
+            }
+
+        }
+    });
