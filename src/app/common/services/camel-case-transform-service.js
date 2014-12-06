3c72f89176d8c050c7c460204529b88cf3f9e424
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-06 20:40:19 +0100

    feat(camel-case): Add camel case transform service and update humps-interceptor-service.

---
+++ src/app/common/services/camel-case-transform-service.js
@@ -0,0 +1,25 @@
+angular
+    .module("common")
+    .service("CamelCaseTransform", function () {
+
+        this.TRANSFORMATION_TYPE = {
+            CAMELIZE: "CAMELIZE",
+            DECAMELIZE: "DECAMELIZE"
+        };
+
+        this.transform = function (that, transformationType) {
+            var thisService = this;
+
+            if ( that && typeof that === 'object' ) {
+                _.each(that, function (value, key) {
+                    if ( that.hasOwnProperty(key) ) {
+                        var newKey = transformationType === thisService.TRANSFORMATION_TYPE.CAMELIZE ? humps.camelize(key) : humps.decamelize(key);
+                        if ( key !== newKey ) {
+                            that[newKey] = that[key];
+                            delete that[key];
+                        }
+                    }
+                });
+            }
+        };
+    });
--- src/app/common/services/humps-interceptor-service.js
+++ src/app/common/services/humps-interceptor-service.js
@@ -1,37 +1,17 @@
 angular
     .module("common")
-    .factory("HumpsInterceptor", function () {
-
-        var TRANSFORMATION_TYPE = {
-            CAMELIZE: "CAMELIZE",
-            DECAMELIZE: "DECAMELIZE"
-        };
-
-        var transform = function (value, transformationType) {
-            if ( value && typeof value === 'object' ) {
-                _.each(value, function (value, key) {
-                    if ( value.hasOwnProperty(key) ) {
-                        var newKey = transformationType === TRANSFORMATION_TYPE.CAMELIZE
-                            ? humps.camelize(key) : humps.decamelize(key);
-                        if ( key !== newKey ) {
-                            value[newKey] = value[key];
-                            delete value[key];
-                        }
-                    }
-                });
-            }
-        };
+    .factory("HumpsInterceptor", function (CamelCaseTransform) {
 
         return {
 
             request: function (config) {
-                transform(config.data, TRANSFORMATION_TYPE.DECAMELIZE);
+                CamelCaseTransform.transform(config.data, CamelCaseTransform.TRANSFORMATION_TYPE.DECAMELIZE);
 
                 return config;
             },
 
             response: function (response) {
-                transform(response.data, TRANSFORMATION_TYPE.CAMELIZE);
+                CamelCaseTransform.transform(response.data, CamelCaseTransform.TRANSFORMATION_TYPE.CAMELIZE);
 
                 return response;
             }
