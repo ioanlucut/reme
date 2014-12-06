8395524bcc50b9f5ab3995b448f8021b65b7a7f9
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-06 20:47:45 +0100

    update(signup): Update sign up controller.

--- src/app/account/controllers/signup-controller.js
+++ src/app/account/controllers/signup-controller.js
@@ -6,6 +6,14 @@ angular
     .controller("SignUpCtrl", function ($scope, AuthService, StatesHandler, User) {
 
         /**
+         * Flag which tells if the sign up action went well or not.
+         * @type {boolean}
+         */
+        $scope.isSignUpErrorOcurred = false;
+
+        $scope.errorMessages = "";
+
+        /**
          * Sign up user information.
          * @type {{firstName: string, lastName: string, email: string, password: string}}
          */
@@ -28,14 +36,24 @@ angular
                 $scope.signUpData.timezone = jstz.determine().name();
 
                 // Create a new user
-                User.$create(signUpData)
-                    .$then(function () {
+                User.$new().$create(signUpData)
+                    .then(function () {
+                        $scope.isSignUpErrorOcurred = false;
 
                         // Log in the user
                         AuthService.login(signUpData.email, signUpData.password);
 
                         // Go home
                         StatesHandler.goHome();
+                    }).catch(function (response) {
+
+                        $scope.errorMessages = response.data && response.data.errors;
+
+                        if ( _.isEmpty($scope.errorMessages) ) {
+                            $scope.errorMessages = ["We encountered a small problem. Please be patient, we come back to you."]
+                        }
+
+                        $scope.isSignUpErrorOcurred = true;
                     });
             }
             else {
