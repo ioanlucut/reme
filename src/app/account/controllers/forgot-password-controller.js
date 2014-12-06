8e2ae1d240271a340b7e5e0f318f90d4bb488005
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-06 20:51:07 +0100

    update(forgot-password-controller) - better handling of the error messages.

--- src/app/account/controllers/forgot-password-controller.js
+++ src/app/account/controllers/forgot-password-controller.js
@@ -6,6 +6,14 @@ angular
     .controller("ForgotPasswordCtrl", function ($state, $scope, AuthService, AUTH_EVENTS, ACCOUNT_FORM_STATE, AccountFormToggle) {
 
         /**
+         * Flag which tells if the authentication went well or not.
+         * @type {boolean}
+         */
+        $scope.isRequestPasswordErrorOcurred = false;
+
+        $scope.errorMessages = "";
+
+        /**
          * Request password reset up user information.
          */
         $scope.forgotPasswordData = {
@@ -19,7 +27,12 @@ angular
             if ( $scope.forgotPasswordForm.$valid ) {
                 AuthService.requestPasswordReset($scope.forgotPasswordData.email)
                     .then(function () {
+                        $scope.isRequestPasswordErrorOcurred = false;
                         AccountFormToggle.setState(ACCOUNT_FORM_STATE.forgotPasswordEmailSent);
+                    }).catch(function (response) {
+                        $scope.isRequestPasswordErrorOcurred = true;
+
+                        $scope.errorMessages = response.data && response.data.errors;
                     });
             }
             else {
