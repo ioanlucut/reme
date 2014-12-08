9a83ce299063ca039c98910913d7fba6821157b6
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-08 18:33:17 +0100

    import(feedback) - Import feedback module from old reme.io.

---
+++ src/app/feedback/controllers/feedback-modal-controller.js
@@ -0,0 +1,49 @@
+/* Feedback modal controller */
+
+angular
+    .module("feedback")
+    .controller("FeedbackModalCtrl", ["$scope", "$timeout", "FeedbackModal", "FeedbackMessage", function ($scope, $timeout, FeedbackModal, FeedbackMessage) {
+
+        $scope.isSending = false;
+        $scope.isSent = false;
+        $scope.feedbackMessage = new FeedbackMessage();
+
+        // Focus the first input when modal is opened
+        FeedbackModal.modalInstance.opened.then($.proxy(function () {
+            $scope.isOpen = true;
+        }, this));
+
+        // Send the feedback message, and close the modal
+        $scope.sendAndClose = function (feedbackForm) {
+            if ( feedbackForm.$valid && !$scope.isSending ) {
+
+                // Form is valid, send the message
+                $scope.isSending = true;
+                $scope.feedbackMessage.send().
+
+                    then(function () {
+                        $scope.isSent = true;
+
+                        // Wait 2 seconds, and close the modal
+                        $timeout(function () {
+                            FeedbackModal.modalInstance.close();
+                        }, 2500);
+                    }).
+
+                    finally(function () {
+                        $scope.isSending = false;
+                        $scope.isOpen = false;
+                    });
+            }
+            else {
+                // Form is invalid
+                feedbackForm.submitted = true;
+            }
+        };
+
+        $scope.cancel = function () {
+            FeedbackModal.modalInstance.dismiss("cancel");
+            $scope.isOpen = false;
+        };
+
+    }]);
---
+++ src/app/feedback/services/feedback-modal-service.js
@@ -0,0 +1,21 @@
+/* Feedback modal */
+
+angular
+    .module("feedback")
+    .service("FeedbackModal", ["$modal", function ($modal) {
+
+        // Init modal instance
+        this.modalInstance = null;
+
+        // Init the feedback modal window
+        this.open = function () {
+
+            // Create modal instance
+            this.modalInstance = $modal.open({
+                templateUrl: "app/feedback/partials/feedbackModal/feedbackModal.html",
+                controller: "FeedbackModalCtrl",
+                windowClass: "modal-feedback"
+            });
+        };
+
+    }]);
