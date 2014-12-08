ab0889944499e73d62c9d6d00a33b51ebfb6c358
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-08 16:07:12 +0100

    refactor(profile) - Functionality updated - before was pretty broken :).

--- src/app/account/controllers/profile-controller.js
+++ src/app/account/controllers/profile-controller.js
@@ -5,7 +5,17 @@ angular
     .module("account")
     .controller("ProfileCtrl", function ($q, $scope, $rootScope) {
 
+        /**
+         * Current user.
+         * @type {$rootScope.currentUser|*}
+         */
         $scope.user = $rootScope.currentUser;
+
+        /**
+         * Error messages.
+         * @type {string}
+         */
+        $scope.errorMessages = "";
 
         /**
          * Profile user information
@@ -25,17 +35,25 @@ angular
             if ( $scope.profileForm.$valid ) {
 
                 // Update the user
-                $q.all($scope.user.$save(profileData))
-                    .then($scope.user.$refresh())
+                $scope.user.$save(profileData)
+
                     .then(function () {
-                        // Set for updated to true
-                        $scope.profileForm.updated = true;
+                        $scope.user.$refresh().then(function () {
+                            // Set form to pristine
+                            $scope.profileForm.$setPristine();
+                            $scope.errorMessages = "";
 
-                        // Set form to pristine
-                        $scope.profileForm.$setPristine();
-                    })
-                    .catch(function (error) {
+                            // Set for updated to true
+                            $scope.profileForm.updated = true;
+                        });
+                    }).catch(function (response) {
+                        $scope.errorMessages = response.data && response.data.errors && response.data.errors.email;
 
+                        if ( _.isEmpty($scope.errorMessages) ) {
+                            $scope.errorMessages = ["We encountered a small problem. Please be patient, we come back to you."]
+                        }
+
+                        $scope.isProfileUpdated = true;
                     });
             }
             else {
