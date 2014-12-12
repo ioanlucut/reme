5392a35dc65a5cd3e4a02bf3d9b20a5eb58c8e21
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 12:51:17 +0100

    feat(reminder) - Set up reminder model.

--- src/app/reminders/services/reminder.js
+++ src/app/reminders/services/reminder.js
@@ -1,184 +1,84 @@
-/* Reminder  */
-
 angular
     .module("reminders")
-    .factory("Reminder", ["$http", "$q", "$", "URLTo", function ($http, $q, $, URLTo) {
+    .factory("Reminder", function ($q, $http, ReminderService) {
+        return {
 
-        function Reminder(attrs) {
+            build: function (loadFromModel) {
 
-            // Setup
-            this.attributes = {};
-            this.idAttribute = "secure_id";
+                var newReminder = {
 
-            // Initialize
-            this.init = function (attrs) {
-                attrs && this.attrs(attrs);
-            };
+                    /**
+                     * Reminder model (DTO)
+                     */
+                    model: {
+                        reminderId: "",
+                        text: "",
+                        dueOn: "",
+                        timezone: "",
+                        additionalAddressees: [],
+                        createdBy: "",
+                        sent: "",
+                        createdAt: "",
+                        updatedAt: ""
+                    },
 
-            // Check if the reminder is new
-            this.isNew = function () {
-                return this.attr(this.idAttribute) === undefined;
-            };
+                    isNew: function () {
+                        return this.reminderId !== "";
+                    },
 
-            // Get or Set an attribute by name
-            this.attr = function (attribute, value) {
+                    loadFrom: function (loadFromModel) {
+                        this.copyKeysFromTo(loadFromModel, this.model);
 
-                // Setter
-                if ( value !== undefined ) {
-                    var mutator = "set" + this.underscoreToCamelCase(attribute) + "Attribute";
-                    if ( $.isFunction(this[mutator]) ) {
+                        return this;
+                    },
 
-                        // There exists a mutator, use it to set the attribute
-                        this[mutator](value);
-                    } else {
+                    update: function (fromData) {
+                        var toBeSaved = {};
+                        this.copyKeysFromTo(fromData || this.model, toBeSaved);
+                        toBeSaved["dueOn"] = toBeSaved["dueOn"].format("{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}");
 
-                        // No mutator exists for this attribute, set it manually
-                        this.attributes[attribute] = value;
+                        return ReminderService.updateReminder(toBeSaved);
+                    },
+
+                    create: function (fromData) {
+                        var toBeCreated = {};
+                        this.copyKeysFromTo(fromData || this.model, toBeCreated);
+                        toBeCreated["dueOn"] = toBeCreated["dueOn"].format("{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}");
+
+                        return ReminderService.createReminder(toBeCreated);
+                    },
+
+                    fetch: function (reminderId) {
+                        var that = this;
+                        var deferred = $q.defer();
+
+                        ReminderService
+                            .getDetails(reminderId || that.model.reminderId)
+                            .then(function (response) {
+                                that.copyKeysFromTo(response, that.model);
+
+                                deferred.resolve(that);
+                                return response;
+                            })
+                            .catch(function (response) {
+                                return $q.reject(response);
+                            });
+
+                        return deferred.promise;
+                    },
+
+                    copyKeysFromTo: function (sourceObject, targetObject, skipKeys) {
+                        _.each(_.keys(this.model), function (key) {
+                            if ( !(skipKeys && _.contains(skipKeys, key)) ) {
+                                targetObject[key] = sourceObject[key];
+                            }
+                        });
                     }
-                }
 
-                // Getter
-                var accessor = "get" + this.underscoreToCamelCase(attribute) + "Attribute";
-                if ( $.isFunction(this[accessor]) ) {
+                };
 
-                    // There exists an accessor, use it to get the attribute
-                    return this[accessor](this.attributes[attribute]);
-                } else {
-
-                    // No accessor exists for this attribute, get it manually
-                    return this.attributes[attribute];
-                }
-            };
-
-            // Get or Set multiple attributes at once
-            this.attrs = function (attrs) {
-
-                var key;
-
-                // Setter
-                if ( attrs !== undefined ) {
-                    for ( key in attrs ) {
-                        this.attr(key, attrs[key]);
-                    }
-                }
-
-                // Getter
-                attrs = {};
-
-                for ( key in this.attributes ) {
-                    attrs[key] = this.attr(key);
-                }
-
-                return attrs;
-            };
-
-            // Mutator for the due_on attribute
-            this.setDueOnAttribute = function (dueOn) {
-                this.attributes.due_on = Date.create(dueOn);
-            };
-
-            // Accessor for the due_on attribute
-            this.getDueOnAttribute = function (dueOn) {
-                return dueOn.format("{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}");
-            };
-
-            // Accessor for the text attribute
-            this.getTextAttribute = function (text) {
-                return $.trim(text.split("@")[0]);
-            };
-
-            // Save reminder
-            this.save = function () {
-
-                var deferred = $q.defer();
-                var url = this.isNew()
-                    ? URLTo.api(this.constructor.urlRoot)
-                    : URLTo.api(this.constructor.urlRoot + "/$1", [this.attr("secure_id")]);
-
-                var method = this.isNew() ? "POST" : "PUT";
-
-                // Send request
-                $http({data: this.attrs(), method: method, url: url}).
-
-                    success($.proxy(function (data) {
-
-                        // Update reminder
-                        this.attrs(data);
-                        deferred.resolve(this);
-
-                    }, this)).
-
-                    error(function (data) {
-
-                        // Reject the deferred with error data
-                        deferred.reject(data);
-                    });
-
-                // Return the promise
-                return deferred.promise;
-            };
-
-            // Delete reminder
-            this.destroy = function () {
-
-                // Make sure the reminder exists
-                if ( this.isNew() ) return false;
-
-                var url = URLTo.api(this.constructor.urlRoot + "/$1", [this.attr("secure_id")]);
-                var method = "DELETE";
-
-                return $http({method: method, url: url});
-            };
-
-            // Convert a string with underscores to camel case
-            this.underscoreToCamelCase = function (str) {
-                str = str.replace(/(\_[a-z])/g, function ($1) {
-                    return $1.toUpperCase().replace('_', '');
-                });
-
-                return str.charAt(0).toUpperCase() + str.slice(1);
-            };
-
-            // Initialize
-            this.init(attrs);
-        }
-
-        // Constructor methods and properties
-        Reminder = angular.extend(Reminder, {
-
-            // The url root to API urls
-            urlRoot: "reminders",
-
-            // Find a reminder by it's secure id
-            find: function (secureId) {
-
-                var deferred = $q.defer();
-
-                // Load the reminder from the server
-                var url = URLTo.api(this.urlRoot + "/$1", [secureId]);
-                var method = "GET";
-
-                $http({method: method, url: url}).
-
-                    success(function (data) {
-
-                        // Resolve the deferred with the reminder
-                        var reminder = new Reminder(data);
-                        deferred.resolve(reminder);
-                    }).
-
-                    error(function (data) {
-
-                        // Reject the deferred with error data
-                        deferred.reject(data);
-                    });
-
-                // Return the promise
-                return deferred.promise;
+                return newReminder.loadFrom(loadFromModel || {});
             }
 
-        });
-
-        return Reminder;
-
-    }]);
+        }
+    });
