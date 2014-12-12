6d3db4830f2c8e49483521952854f84d045b822a
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 18:01:37 +0100

    feat(friendly-hour-filter) - Add.

---
+++ src/app/common/filters/friendly-hour-filter.js
@@ -0,0 +1,10 @@
+/* Friendly date filter */
+
+angular
+    .module("common")
+    .filter('friendlyHour', function () {
+        return function (date) {
+
+            return moment(date).format("h:mm");
+        };
+    });
