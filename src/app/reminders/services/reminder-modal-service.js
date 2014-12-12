3d3e66e2a1e7c0b6111f2bd4f631b6a9a69c4db1
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 12:50:37 +0100

    feat(reminder-modal-service) - Add basic functionality.

---
+++ src/app/reminders/services/reminder-modal-service.js
@@ -0,0 +1,34 @@
+/* Feedback modal */
+
+angular
+    .module("reminders")
+    .service("ReminderModalService", ["$modal", function ($modal) {
+
+        // Init modal instance
+        this.modalInstance = null;
+
+        // Init the feedback modal window
+        this.open = function () {
+
+            // Create modal instance
+            this.modalInstance = $modal.open({
+                templateUrl: "app/reminders/partials/reminderModal/reminderModal.html",
+                controller: "ReminderModalCtrl",
+                windowClass: "modal-feedback",
+                resolve: {
+                    reminder: ["$window", "Reminder", "jstz", function ($window, Reminder, jstz) {
+                        // Make the reminder due the next hour
+                        var defaultDueOn = Date.create().addHours(1).set({minute: 0, second: 0});
+
+                        return new Reminder.build({
+                            text: "",
+                            dueOn: defaultDueOn,
+                            timezone: jstz.determine().name(),
+                            additionalAddressees: []
+                        });
+                    }]
+                }
+            });
+        };
+
+    }]);
