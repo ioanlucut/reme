a7ab841b3f5d8e46cf29318d96ee68b9d936a0e9
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 16:14:30 +0100

    refactor(reminder-list-controller) - Updated business logic.

--- src/app/reminders/controllers/reminder-list-controller.js
+++ src/app/reminders/controllers/reminder-list-controller.js
@@ -3,6 +3,15 @@
  */
 angular
     .module("reminders")
-    .controller("ReminderListCtrl", function ($scope, reminderList) {
+    .controller("ReminderListCtrl", function ($scope, reminderList, ReminderDeleteModalService) {
         $scope.reminderList = reminderList;
+
+        $scope.cancel = function () {
+            ReminderDeleteModalService.modalInstance.dismiss("cancel");
+            $scope.isOpen = false;
+        };
+
+        $scope.openDeleteReminderModalService = function (reminder) {
+            ReminderDeleteModalService.open(reminder.reminderId);
+        };
     });
