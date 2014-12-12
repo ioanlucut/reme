3e11730b67e829e799ae7bccdec47072cf3c40e2
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 12:53:01 +0100

    feat(reminders-constants).

---
+++ src/app/reminders/constants/reminders-constants.js
@@ -0,0 +1,14 @@
+/**
+ * Reminders constants.
+ */
+angular
+    .module("reminders")
+    .constant("REMINDER_URLS", {
+        create: "reminders",
+        update: "reminders/:reminderId",
+        details: "reminders/:reminderId",
+        delete: "reminders/:reminderId",
+        allReminders: "reminders",
+        pastReminders: "reminders/past?:local_time&:local_time_zone",
+        upcomingReminders: "reminders/upcoming?:local_time&:local_time_zone"
+    });
