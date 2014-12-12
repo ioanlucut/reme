886dbc7054e786736430f09fdeb2ee2ebe5beed9
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-12 12:51:46 +0100

    feat(reminders) - Set up main reminders page using multiple nested views.

--- src/app/reminders/reminders.js
+++ src/app/reminders/reminders.js
@@ -17,101 +17,36 @@ angular
 
         $stateProvider
 
-            // Home page
             .state("reminders", {
                 url: "/reminders",
-                templateUrl: "app/reminders/partials/reminders.html",
-                controller: "RemindersCtrl",
-                title: "Reminders"
-            })
+                views: {
 
-            // Reminder create
-            .state("create", {
-                url: "/create",
-                templateUrl: "app/reminders/partials/reminder/create.html",
-                abstract: true
-            })
+                    '': { templateUrl: 'app/reminders/partials/reminder/reminders.html' },
 
-            // Reminder create form
-            .state("create.form", {
-                url: "", // See http://stackoverflow.com/a/17004670/240324
-                templateUrl: "app/reminders/partials/reminder/form.html",
-                resolve: {
-                    reminder: ["$window", "Reminder", "jstz", function ($window, Reminder, jstz) {
+                    'create@reminders': {
+                        templateUrl: "app/reminders/partials/reminder/reminders.create.html",
+                        controller: "ReminderCtrl"
+                    },
 
-                        // Make the reminder due the next hour
-                        var defaultDueOn = Date.create().addHours(1).set({minute: 0, second: 0});
+                    'list@reminders': {
+                        templateUrl: "app/reminders/partials/reminder/reminders.list.html",
+                        controller: "ReminderListCtrl",
+                        resolve: {
+                            reminderList: function ($q, ReminderService) {
+                                var deferred = $q.defer();
+                                ReminderService
+                                    .getAllReminders()
+                                    .then(function (response) {
+                                        deferred.resolve(response);
+                                    }).catch(function () {
+                                        console.log('error');
+                                        deferred.resolve("nix");
+                                    });
 
-                        return new Reminder({
-                            text: "",
-                            due_on: defaultDueOn,
-                            created_by: $window.localStorage.reminderCreatedBy || "",
-                            additional_addressees: [],
-                            timezone: jstz.determine().name()
-                        });
-                    }]
-                },
-                controller: "ReminderFormCtrl",
-                title: "Create email reminders in seconds - Reme.IO"
-            }).
-
-            // Reminder update
-            state("update", {
-                url: "/reminders/:secureId/update",
-                templateUrl: "app/reminders/partials/reminder/update.html",
-                abstract: true
-            }).
-
-            // Reminder update form
-            state("update.form", {
-                url: "",
-                templateUrl: "app/reminders/partials/reminder/form.html",
-                resolve: {
-                    reminder: ["$location", "$stateParams", "$q", "Reminder", "ReminderCache", function ($location, $stateParams, $q, Reminder, ReminderCache) {
-
-                        var deferred = $q.defer();
-                        Reminder.find($stateParams["secureId"]).
-
-                            then(function (reminder) {
-                                ReminderCache.put(reminder.attr("secure_id"), reminder);
-                                deferred.resolve(reminder);
-                            }).
-
-                            catch(function () {
-                                $location.path("/");
-                            });
-
-                        return deferred.promise;
-                    }]
-                },
-                controller: "ReminderFormCtrl",
-                title: "Update your reminder  - Reme.IO"
-            }).
-
-            // Reminder created
-            state("saved", {
-                url: "/reminders/:secureId/saved",
-                templateUrl: "app/reminders/partials/saved.html",
-                resolve: {
-                    reminder: ["$location", "$stateParams", "ReminderCache", function ($location, $stateParams, ReminderCache) {
-
-                        var reminder = ReminderCache.get($stateParams["secureId"]);
-                        if ( !reminder ) {
-                            $location.path("/");
+                                return deferred.promise;
+                            }
                         }
-
-                        return reminder;
-                    }]
-                },
-                controller: "ReminderSavedCtrl",
-                title: "Reminder saved - Reme.IO"
-            }).
-
-            // Privacy policy
-            state("privacy", {
-                url: "/privacy",
-                templateUrl: "app/reminders/partials/privacy.html",
-                title: "Privacy Policy - Reme.IO"
+                    }
+                }
             });
-
     }]);
