ec71d9154aa6da39eda5cbc4f25c6993c7116c1e
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-15 00:30:11 +0100

    refactor(reminder-list-controller) - Improve logic and add broadcasting  listening events logic of deleted and created reminder.

--- src/app/reminders/controllers/reminder-list-controller.js
+++ src/app/reminders/controllers/reminder-list-controller.js
@@ -3,7 +3,7 @@
  */
 angular
     .module("reminders")
-    .controller("ReminderListCtrl", function ($scope, reminderList, ReminderDeleteModalService) {
+    .controller("ReminderListCtrl", function ($scope, reminderList, ReminderDeleteModalService, REMINDER_EVENTS, $log, flash) {
         $scope.reminderList = reminderList;
 
         $scope.cancel = function () {
@@ -14,4 +14,29 @@ angular
         $scope.openDeleteReminderModalService = function (reminder) {
             ReminderDeleteModalService.open(reminder.reminderId);
         };
+
+        /**
+         * On reminder created, display a success message, and add reminder to the list.
+         */
+        $scope.$on(REMINDER_EVENTS.isCreated, function (event, args) {
+            flash.success = args.message;
+
+            $scope.reminderList.push(args.reminder);
+        });
+
+        /**
+         * On reminder deleted, display a success message, and remove the reminder from the list.
+         */
+        $scope.$on(REMINDER_EVENTS.isDeleted, function (event, args) {
+            flash.success = args.message;
+
+            _.remove($scope.reminderList, function (reminderFromArray) {
+                var reminderId = _.parseInt(args.reminder.reminderId, 10);
+                if ( _.isNaN(reminderId) ) {
+                    return false;
+                }
+
+                return reminderFromArray.reminderId === reminderId;
+            });
+        });
     });
