53ef46608ff6d82cbd13dd30aac4c77d3bd1d7ad
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-15 00:29:35 +0100

    refactor(reminder-modal-controller) - Improve logic and add broadcasting logic of deleted reminder.

--- src/app/reminders/controllers/reminder-delete-modal-controller.js
+++ src/app/reminders/controllers/reminder-delete-modal-controller.js
@@ -1,6 +1,6 @@
 angular
     .module("reminders")
-    .controller("ReminderDeleteModalCtrl", ["$scope", "$rootScope", "$stateParams", "$window", "$", "URLTo", "ReminderDeleteModalService", "reminder", "$timeout", "StatesHandler", function ($scope, $rootScope, $stateParams, $window, $, URLTo, ReminderDeleteModalService, reminder, $timeout, StatesHandler) {
+    .controller("ReminderDeleteModalCtrl", ["$scope", "$rootScope", "$stateParams", "$window", "$", "URLTo", "ReminderDeleteModalService", "reminder", "$timeout", "StatesHandler", "REMINDER_EVENTS", function ($scope, $rootScope, $stateParams, $window, $, URLTo, ReminderDeleteModalService, reminder, $timeout, StatesHandler, REMINDER_EVENTS) {
 
         /**
          * Reminder to be created (injected with few default values)
@@ -30,12 +30,19 @@ angular
 
                 // Destroy reminder
                 $scope.reminder.destroy()
-                    .then(function () {
+                    .then(function (reminderAsResponse) {
 
                         // Wait 2 seconds, and close the modal
                         $timeout(function () {
                             ReminderDeleteModalService.modalInstance.close();
                         }, 1000);
+
+                        $timeout(function () {
+                            $rootScope.$broadcast(REMINDER_EVENTS.isDeleted, {
+                                reminder: $scope.reminder.model,
+                                message: 'Reminder successfully deleted!'
+                            });
+                        }, 1500);
                     })
                     .catch(function () {
 
