ddd3fb05e7e356e739a91319c1d0fb5e2e5b78f4
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-16 22:40:12 +0100

    feat(reminder-list-controller) -Integrate update reminder.

--- src/app/reminders/controllers/reminder-list-controller.js
+++ src/app/reminders/controllers/reminder-list-controller.js
@@ -3,16 +3,29 @@
  */
 angular
     .module("reminders")
-    .controller("ReminderListCtrl", function ($scope, reminderList, ReminderDeleteModalService, REMINDER_EVENTS, $log, flash) {
+    .controller("ReminderListCtrl", function ($scope, reminderList, ReminderDeleteModalService, ReminderUpdateModalService, REMINDER_EVENTS, $log, flash) {
         $scope.reminderList = reminderList;
 
         $scope.cancel = function () {
             ReminderDeleteModalService.modalInstance.dismiss("cancel");
+            ReminderDeleteModalService.modalInstance.dismiss("cancel");
             $scope.isOpen = false;
         };
 
+        /**
+         * Open DELETE modal
+         * @param reminder
+         */
         $scope.openDeleteReminderModalService = function (reminder) {
             ReminderDeleteModalService.open(reminder.reminderId);
+        };
+
+        /**
+         * Open UPDATE modal
+         * @param reminder
+         */
+        $scope.openUpdateReminderModalService = function (reminder) {
+            ReminderUpdateModalService.open(reminder.reminderId);
         };
 
         /**
@@ -20,6 +33,19 @@ angular
          */
         $scope.$on(REMINDER_EVENTS.isCreated, function (event, args) {
             flash.success = args.message;
+
+            $scope.reminderList.push(args.reminder);
+        });
+
+        /**
+         * On reminder updated, simply display the message.
+         */
+        $scope.$on(REMINDER_EVENTS.isUpdated, function (event, args) {
+            flash.success = args.message;
+
+            _.remove($scope.reminderList, function (reminder) {
+                return reminder.reminderId === args.reminder.reminderId
+            });
 
             $scope.reminderList.push(args.reminder);
         });
