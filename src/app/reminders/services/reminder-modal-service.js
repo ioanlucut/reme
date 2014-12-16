96a96dff261949f088f7156801b4e78ee3e2d955
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-16 22:41:05 +0100

    fix(reminder-modal-service) -Typo fix.

--- src/app/reminders/services/reminder-modal-service.js
+++ src/app/reminders/services/reminder-modal-service.js
@@ -24,7 +24,7 @@ angular
                             text: "",
                             dueOn: defaultDueOn,
                             timezone: jstz.determine().name(),
-                            additionalAddressees: []
+                            additionalAddresses: []
                         });
                     }]
                 }
