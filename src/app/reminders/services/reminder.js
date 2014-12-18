feb4a73b13a445db8ba5e1ea094c08d7115ba06e
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-18 06:41:14 +0100

    refactor(reminder) - Built a more reliable dto and business object to represent the reminder.

--- src/app/reminders/services/reminder.js
+++ src/app/reminders/services/reminder.js
@@ -1,117 +1,104 @@
 angular
     .module("reminders")
-    .factory("Reminder", function ($q, $http, ReminderService) {
-        return {
+    .factory("Reminder", function ($q, $http, ReminderService, ReminderTransformerService) {
 
-            build: function (modelToLoadFrom) {
+        /**
+         * Reminder class.
+         * @constructor
+         */
+        function Reminder() {
 
-                var newReminder = {
+            /**
+             * Represents the DTO model of the reminder.
+             */
+            this.model = {
+                reminderId: "",
+                text: "",
+                dueOn: "",
+                timezone: "",
+                additionalAddresses: [],
+                createdBy: "",
+                sent: "",
+                createdAt: "",
+                updatedAt: ""
+            };
 
-                    /**
-                     * Reminder model (DTO)
-                     */
-                    model: {
-                        reminderId: "",
-                        text: "",
-                        dueOn: "",
-                        timezone: "",
-                        additionalAddresses: [],
-                        createdBy: "",
-                        sent: "",
-                        createdAt: "",
-                        updatedAt: ""
-                    },
+            /**
+             * Is reminder new.
+             * @returns {boolean}
+             */
+            this.isNew = function () {
+                return this.model.reminderId === "" || _.isUndefined(this.model.reminderId);
+            };
 
-                    isNew: function () {
-                        return this.model.reminderId === undefined;
-                    },
+            /**
+             * Saves a reminder and update model with response.
+             * @returns {*}
+             */
+            this.save = function () {
+                var that = this;
+                var reminderDto = ReminderTransformerService.toReminderDto(this);
 
-                    /**
-                     * Saves a reminder and update model with response.
-                     * @returns {*}
-                     */
-                    save: function () {
-                        var that = this;
-                        var modelToBeSaved = {};
+                var deferred = $q.defer();
+                ReminderService
+                    [this.isNew() ? 'createReminder' : 'updateReminder'](reminderDto)
+                    .then(function (response) {
+                        ReminderTransformerService.toReminder(response, that);
+                        deferred.resolve(that);
+                        return response;
+                    })
+                    .catch(function (response) {
+                        return $q.reject(response);
+                    });
 
-                        this.copyKeysFromTo(this.model, modelToBeSaved);
-                        modelToBeSaved["dueOn"] = modelToBeSaved["dueOn"].format("{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}");
-                        modelToBeSaved["additionalAddresses"] = modelToBeSaved["additionalAddresses"].join(",");
+                return deferred.promise;
+            };
 
-                        var deferred = $q.defer();
-                        ReminderService
-                            [this.isNew() ? 'createReminder' : 'updateReminder'](modelToBeSaved)
-                            .then(function (response) {
-                                that.parseFromTo(response, that);
+            /**
+             * Fetches and updates existing reminder.
+             * @param reminderId
+             * @returns {*}
+             */
+            this.fetch = function (reminderId) {
+                var that = this;
+                var deferred = $q.defer();
 
-                                deferred.resolve(that);
-                                return response;
-                            })
-                            .catch(function (response) {
-                                return $q.reject(response);
-                            });
+                ReminderService
+                    .getDetails(reminderId || that.model.reminderId)
+                    .then(function (response) {
+                        ReminderTransformerService.toReminder(response, that);
+                        deferred.resolve(that);
+                        return response;
+                    })
+                    .catch(function (response) {
+                        return $q.reject(response);
+                    });
 
-                        return deferred.promise;
-                    },
+                return deferred.promise;
+            };
 
-                    fetch: function (reminderId) {
-                        var that = this;
-                        var deferred = $q.defer();
-
-                        ReminderService
-                            .getDetails(reminderId || that.model.reminderId)
-                            .then(function (response) {
-                                that.parseFromTo(response, that);
-
-                                deferred.resolve(that);
-                                return response;
-                            })
-                            .catch(function (response) {
-                                return $q.reject(response);
-                            });
-
-                        return deferred.promise;
-                    },
-
-                    destroy: function () {
-                        return ReminderService.deleteReminder(this.model);
-                    },
-
-                    parseFromTo: function (sourceObject) {
-                        if ( !sourceObject ) {
-                            return this;
-                        }
-
-                        this.copyKeysFromTo(sourceObject, this.model);
-
-                        // handle date conversion
-                        if ( this.model["dueOn"] ) {
-                            this.model["dueOn"] = moment(this.model["dueOn"]).toDate();
-                        }
-                        //handle addresses conversion
-                        var additionAddresses = this.model["additionalAddresses"];
-                        if ( _.isEmpty(additionAddresses) ) {
-                            this.model["additionalAddresses"] = [];
-                        }
-                        else {
-                            this.model["additionalAddresses"] = _.values(additionAddresses.split(","));
-                        }
-
-                        return this;
-                    },
-
-                    copyKeysFromTo: function (sourceObject, targetObject, skipKeys) {
-                        _.each(_.keys(this.model), function (key) {
-                            if ( !(skipKeys && _.contains(skipKeys, key)) ) {
-                                targetObject[key] = sourceObject[key];
-                            }
-                        });
-                    }
-
-                };
-
-                return newReminder.parseFromTo(modelToLoadFrom || {}, newReminder);
-            }
+            /**
+             * Destroys (deletes) a reminder.
+             * @returns {*}
+             */
+            this.destroy = function () {
+                return ReminderService.deleteReminder(this.model);
+            };
 
         }
+
+        /**
+         * Builds a reminder with given data.
+         * @param data
+         * @returns {Reminder}
+         */
+        Reminder.build = function (data) {
+            if ( _.isEmpty(data) ) {
+                return new Reminder();
+            }
+
+            return ReminderTransformerService.toReminder(data, new Reminder());
+        };
+
+        return Reminder;
     });
