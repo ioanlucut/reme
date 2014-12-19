37f2e8082df1e08d167f288884e3fcd718c4f100
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-19 01:43:21 +0100

    refactor(reminder*) - Removed reminder fetch/create/update login in transformer and perform transform to dto and back directly in reminders services.

--- src/app/reminders/reminders.js
+++ src/app/reminders/reminders.js
@@ -32,12 +32,12 @@ angular
                         templateUrl: "app/reminders/partials/reminder/reminders.list.html",
                         controller: "ReminderListCtrl",
                         resolve: {
-                            reminderList: function ($q, ReminderService, ReminderTransformerService) {
+                            reminderList: function ($q, ReminderService) {
                                 var deferred = $q.defer();
                                 ReminderService
                                     .getAllReminders()
                                     .then(function (response) {
-                                        deferred.resolve(ReminderTransformerService.toReminders(response));
+                                        deferred.resolve(response);
                                     }).catch(function () {
                                         deferred.resolve([]);
                                     });
