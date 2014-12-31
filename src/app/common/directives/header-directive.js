f73ff669b004b4313468a3de8d37e38ab53cacc3
Ionel Lucut <ioan.lucut88@gmail.com>
2014-12-31 15:02:11 +0100

    fix(login/logout) - Email dropdown not showing after login

--- src/app/common/directives/header-directive.js
+++ src/app/common/directives/header-directive.js
@@ -3,12 +3,17 @@
  */
 angular
     .module("common")
-    .directive("header", function () {
+    .directive("header", function ($rootScope) {
         return {
             restrict: "A",
             templateUrl: "app/common/partials/header.html",
             link: function (scope, el) {
 
+                /**
+                 * Reference to the current user.
+                 * @type {$rootScope.currentUser|*}
+                 */
+                scope.currentUser = $rootScope.currentUser;
             }
         };
     });
