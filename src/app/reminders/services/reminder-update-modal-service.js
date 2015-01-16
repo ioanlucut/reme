5634a431e6eaa6bb88a2998c4716956c87d3ccb3
Ionel Lucut <ioan.lucut88@gmail.com>
2015-01-16 17:33:54 +0100

    feat(reminders) - NLP not working #87

--- src/app/reminders/services/reminder-update-modal-service.js
+++ src/app/reminders/services/reminder-update-modal-service.js
@@ -17,7 +17,6 @@ angular
                 windowClass: "modal-feedback",
                 resolve: {
                     reminder: function () {
-                        reminderToBeUpdated.model.dueOn.isDateSetFromUpdate = true;
                         return reminderToBeUpdated;
                     },
                     reminderIndex: function () {
