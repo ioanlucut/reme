8d2bf34c1df17a58eb95fce94c159943c26d5d3c
Ionel Lucut <ioan.lucut88@gmail.com>
2015-01-18 04:48:50 +0100

    feat() - If registration failed (token expired) - the link Request the email again does not work. #104

--- src/app/account/controllers/signup-confirm-invalid-controller.js
+++ src/app/account/controllers/signup-confirm-invalid-controller.js
@@ -1,4 +1,19 @@
 angular
     .module("account")
-    .controller("SignUpConfirmInvalidCtrl", function () {
+    .controller("SignUpConfirmInvalidCtrl", function ($scope, AuthService, StatesHandler) {
+
+        /**
+         * Flag which tells if user is currently authenticated while coming to this page.
+         */
+        $scope.isUserAuthenticated = AuthService.isAuthenticated();
+
+        /**
+         * Continues to reset password page. (try again functionality)
+         */
+        $scope.goHome = function () {
+            if ( $scope.isUserAuthenticated ) {
+                AuthService.logout();
+            }
+            StatesHandler.goHome();
+        }
     });
