636e5b752c97f29fb4ee415fda8369bc089e4702
Ionel Lucut <ioan.lucut88@gmail.com>
2015-01-20 23:11:01 +0100

    2.1.0-RC1 - from DEV

--- src/app/app.env.config.js
+++ src/app/app.env.config.js
@@ -1,11 +1,7 @@
 "use strict";
 
-angular.module('config', [])
+ angular.module('config', [])
 
-    .constant('ENV', {
-        name: 'development',
-        apiEndpoint: 'http://dev-api.reme.io',
-        mixPanelId: '216177bcdddef0cf2edd1650e63a3449'
-    })
+.constant('ENV', {name:'development',apiEndpoint:'http://dev-api.reme.io',mixPanelId:'216177bcdddef0cf2edd1650e63a3449'})
 
 ;
