a119f831736ff0c6c389145d75899f0ef38ed93e
Ionel Lucut <ioan.lucut88@gmail.com>
2015-01-23 21:19:38 +0100

    feat() - Environment detection #103

--- src/app/app.env.config.js
+++ src/app/app.env.config.js
@@ -1,11 +1,7 @@
 "use strict";
 
-angular.module('config', [])
+ angular.module('config', [])
 
-    .constant('ENV', {
-        name: 'development',
-        apiEndpoint: 'http://dev-api.reme.io',
-        mixPanelId: 'e9ba9ca056ce11433777e3c8f59014b4'
-    })
+.constant('ENV', {name:'development',apiEndpoint:'http://dev-api.reme.io',mixPanelId:'e9ba9ca056ce11433777e3c8f59014b4'})
 
 ;
