27f356b4cb9e16599c45b7593737774b37a31ee9
sorinpantis <sorinpantis87@gmail.com>
2015-02-01 15:11:25 +0200

    feat(reminders) - change past reminders header to link

--- src/app/reminders/filter/reminders-header-filter.js
+++ src/app/reminders/filter/reminders-header-filter.js
@@ -2,7 +2,7 @@ angular
     .module("reminders")
     .filter('remindersHeader', function ($sce) {
         return function (text, reverse) {
-            var template = reverse ? '<span class="reminder-list-box__header__past">$1 past reminders</span>' : '<span class="reminder-list-box__header__upcoming">You have $1 upcoming reminders</span>';
+            var template = reverse ? '<a class="reminder-list-box__header__past" href="#">$1 past reminders</a>' : '<span class="reminder-list-box__header__upcoming">You have $1 upcoming reminders</span>';
 
             return $sce.trustAsHtml(template.replace('$1', text || '0'))
         };
