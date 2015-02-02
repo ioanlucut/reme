b0dee8032a9478ef275150829070bea2905f8acc
Ionel Lucut <ioan.lucut88@gmail.com>
2015-02-02 20:39:08 +0100

    fix() - Add a tip to the past reminders link - something like "Click to expand". #152

--- src/app/reminders/filter/reminders-header-filter.js
+++ src/app/reminders/filter/reminders-header-filter.js
@@ -2,9 +2,7 @@ angular
     .module("reminders")
     .filter('remindersHeader', function ($sce) {
         return function (text, reverse) {
-            var template = reverse ?
-                '<a class="reminder-list-box__header__past simptip-position-top simptip-fade simptip-smooth" data-tooltip="Click to expand" href="#">You have $1 past reminders</a>' :
-                '<span class="reminder-list-box__header__upcoming">You have $1 upcoming reminders</span>';
+            var template = reverse ? '<a class="reminder-list-box__header__past" href="#">You have $1 past reminders</a>' : '<span class="reminder-list-box__header__upcoming">You have $1 upcoming reminders</span>';
 
             return $sce.trustAsHtml(template.replace('$1', text || '0'))
         };
